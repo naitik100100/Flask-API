f0cabe2d266fe67b451be55e77d808c075a26178
Krupa Patel <kr635164@dal.ca>
2020-06-23 19:10:55 -0300

    Added GET method for search by username

--- app.py
+++ app.py
@@ -12,20 +12,32 @@ class UserManagement:
         self.email = email
         self.username = username
 
+
 @app.route("/adduser", methods=["POST"])
 def addUser():
     getUserData = request.get_json()
     userObject = UserManagement(getUserData["email"], getUserData["username"])
     UserList.append(userObject)
     # To check whether user is added to the list or not.
-    print(json.dumps({'Users': [ob.__dict__ for ob in UserList]}))
     return jsonify({'message': "User created successfully..."})
 
 @app.route("/getAllUsers", methods=["GET"])
 def getAllUsers():
     return json.dumps({'Users': [ob.__dict__ for ob in UserList]})
     
-    
+ 
+@app.route("/getUser" , methods=["GET"])
+def getUserByUsername():
+    getUserData = request.get_json()
+    temp=[]
+    temp.append({'Users': [ob.__dict__ for ob in UserList]})
+    for user in range(len(temp[0].get('Users'))):
+        if( temp[0].get('Users')[user].get('username') == getUserData["username"]):
+            jsonData = json.dumps(temp[0].get('Users')[user])
+            return jsonData
+    return {'Message' : 'No data found'}
+
+
 @app.route("/moduser", methods=["PUT"])
 def moduser():
     getUserData = request.get_json()
@@ -37,4 +49,3 @@ def moduser():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
     
-  
